c237f5e750f8c22e4bfef6a3ed87da2ca48272d1
Soban Ali <ali_soban@bah.com>
2023-08-07 20:32:06 +0000

    temp

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,8 +13,8 @@ values(123,'Jay-Z', 'jayz@gmail.com','2012-05-12 00:00:00.0');
 insert into REGISTRATIONS (EVENT_ID, REGISTRATION_DATE, NOTES)
 values ('4', '2019-01-15T00:00:00.000+0000', 'omg');
 insert into REGISTRATIONS (EVENT_ID, REGISTRATION_DATE, NOTES)
-values ('1', '2020-01-15T00:00:00.000+0000', 'i think this finally works');
+values ('1', '1', '2020-01-15T00:00:00.000+0000', 'i think this finally works');
 insert into REGISTRATIONS (EVENT_ID, REGISTRATION_DATE, NOTES)
-values ('10', '2021-01-15T00:00:00.000+0000', 'it was seriously just a naming issue');
+values ('2', '10', '2021-01-15T00:00:00.000+0000', 'it was seriously just a naming issue');
 insert into REGISTRATIONS (EVENT_ID, REGISTRATION_DATE, NOTES)
-values ('20', '2022-01-15T00:00:00.000+0000', 'thanks Cameron');
+values ('3', '20', '2022-01-15T00:00:00.000+0000', 'thanks Cameron');
